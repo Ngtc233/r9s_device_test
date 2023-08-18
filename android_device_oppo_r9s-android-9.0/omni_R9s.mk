a7b3f54482f353a92aaff3deee72fbe7aa25fae3
Ngtc233 <89304330+Ngtc233@users.noreply.github.com>
2023-08-18 17:16:20 +0800

    Add files via upload

---
+++ android_device_oppo_r9s-android-9.0/omni_R9s.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+# Encryption
+PRODUCT_PACKAGES += \
+    libcryptfs_hw
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := R9s
+PRODUCT_NAME := omni_R9s
+PRODUCT_BRAND := OPPO
+PRODUCT_MODEL := OPPO R9s
+PRODUCT_MANUFACTURER := oppo
+PRODUCT_RELEASE_NAME := OPPO OPPO R9s
+
+# Build prop overrides
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="R9s" \
+    PRODUCT_NAME="R9s"
